830303ccfaca4d5dec21a8ef852b399f95ad1795
Thu Hiền Hồ <thuhien191197@gmail.com>
2018-06-06 16:15:24 +0700

    on/off light and sign up

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,51 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//================================ FORM ==============================
+var ds1 = "Male";
+var ds2 = "Female";
 
-// var ds1 = "Male";
-// var ds2 = "Female";
 
-// class Form extends React.Component{
-//   render(){
-//     return(
-//         <div>
-//           <div>
-//             username: <input type="text" name="user" /><br /><br />
-//           </div>
+function Success(props) {
+  return <h1>Welcome back!</h1>;
+}
 
-//           <div>
-//             address: <input type="text" name="addr" /><br /><br />
-//           </div>
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
 
-//           <div>
-//             Gender:  
-//             <select>
-//               <option>{this.props.attDS1}</option>
-//               <option>{this.props.attDS2}</option>
-//             </select>
-//           </div>
-//           <br /><br />
-//           <div>
-//             <input type="submit" />
-//           </div>
+class Form extends React.Component{
+  render(){
+    return(
+        <div>
+          <div>
+            username: <input type="text" name="user" /><br /><br />
+          </div>
+
+          <div>
+            Gender:  
+            <select>
+              <option>{this.props.attDS1}</option>
+              <option>{this.props.attDS2}</option>
+            </select>
+          </div>
+          <br /><br />
+          <div>
+            <input type="submit" />
+          </div>
           
-//         </div>
-//     );
-//   }
-// };
+        </div>
+    );
+  }
+};
 
-// class App extends React.Component{
-//   render(){
-//     return(
-//       <div>
-//         <h1>SIGN UP</h1>
-//         <Form attDS1= {this.props.atttDS1} attDS2= {this.props.atttDS2} />
-//       </div>
-//     );
-//   }
-// };
 
 // ReactDOM.render(<App atttDS1={ds1} atttDS2={ds2} />, document.getElementById('root'));
+
+//================================ ON?OFF LIGHT ==============================
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
@@ -57,6 +54,7 @@ class Toggle extends React.Component {
   }
 
   handleClick() {
+    //goi this.setState de thay doi trang thai cua component
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
@@ -64,18 +62,66 @@ class Toggle extends React.Component {
 
   render() {
     return (
-      <div onClick={this.handleClick}>
-        <img src={this.state.isToggleOn ? 'https://www.w3schools.com/js/pic_bulbon.gif' : 'https://www.w3schools.com/js/pic_bulboff.gif'} />
-        <button >
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
+      <div>
+        <div onClick={this.handleClick}>
+          <img src={this.state.isToggleOn ? 'https://www.w3schools.com/js/pic_bulbon.gif' : 'https://www.w3schools.com/js/pic_bulboff.gif'} />
+          <button >
+            {this.state.isToggleOn ? 'ON' : 'OFF'}
+          </button>        
+        </div>
+        
       </div>
-      
+ 
     );
   }
 }
 
+
+
+
+
+//=============================================
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>SIGN UP</h1>
+        <Form attDS1= {this.props.atttDS1} attDS2= {this.props.atttDS2} />
+        <br /> <br /> ==================================================
+        <Toggle />
+      </div>
+    );
+  }
+};
+
 ReactDOM.render(
-  <Toggle />,
+  <App atttDS1={ds1} atttDS2={ds2}/>,
   document.getElementById('root')
 );
+
+
+//========================================
+
+
+// function UserGreeting(props) {
+//   return <h1>Welcome User!</h1>;
+// }
+
+// function GuestGreeting(props) {
+//   return <h1>Please sign up.</h1>;
+// }
+
+// function Greeting(props) {
+//   const isLoggedIn2 = props.isLoggedIn;
+//   if (isLoggedIn2 == 'user') {
+//     return <UserGreeting />;
+//   }
+//   return <GuestGreeting />;
+// }
+
+// ReactDOM.render(
+//   // Try changing to isLoggedIn={true}:
+//   <Greeting isLoggedIn={'user'} />,
+//   document.getElementById('root')
+// );
