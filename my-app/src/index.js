1c7f9c7b332b043caf1bac740e930eddd2a8c642
Thu Hiền Hồ <thuhien191197@gmail.com>
2018-06-06 15:18:10 +0700

    on/off light

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,47 +3,79 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-var ds1 = "Male";
-var ds2 = "Female";
+// var ds1 = "Male";
+// var ds2 = "Female";
 
-class Form extends React.Component{
-  render(){
-    return(
-        <div>
-          <div>
-            username: <input type="text" name="user" /><br /><br />
-          </div>
+// class Form extends React.Component{
+//   render(){
+//     return(
+//         <div>
+//           <div>
+//             username: <input type="text" name="user" /><br /><br />
+//           </div>
 
-          <div>
-            address: <input type="text" name="addr" /><br /><br />
-          </div>
+//           <div>
+//             address: <input type="text" name="addr" /><br /><br />
+//           </div>
 
-          <div>
-            Gender:  
-            <select>
-              <option>{this.props.attDS1}</option>
-              <option>{this.props.attDS2}</option>
-            </select>
-          </div>
-          <br /><br />
-          <div>
-            <input type="submit" />
-          </div>
+//           <div>
+//             Gender:  
+//             <select>
+//               <option>{this.props.attDS1}</option>
+//               <option>{this.props.attDS2}</option>
+//             </select>
+//           </div>
+//           <br /><br />
+//           <div>
+//             <input type="submit" />
+//           </div>
           
-        </div>
-    );
-  }
-};
+//         </div>
+//     );
+//   }
+// };
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>SIGN UP</h1>
-        <Form attDS1= {this.props.atttDS1} attDS2= {this.props.atttDS2} />
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <h1>SIGN UP</h1>
+//         <Form attDS1= {this.props.atttDS1} attDS2= {this.props.atttDS2} />
+//       </div>
+//     );
+//   }
+// };
+
+// ReactDOM.render(<App atttDS1={ds1} atttDS2={ds2} />, document.getElementById('root'));
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        <img src={this.state.isToggleOn ? 'https://www.w3schools.com/js/pic_bulbon.gif' : 'https://www.w3schools.com/js/pic_bulboff.gif'} />
+        <button >
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
       </div>
+      
     );
   }
-};
+}
 
-ReactDOM.render(<App atttDS1={ds1} atttDS2={ds2} />, document.getElementById('root'));
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
